51d701b5a36916d1d7d728674188a3a687a4fcaf
DanielFigueira <daniel.figueira@ist.utl.pt>
2017-03-23 12:38:35 +0000

    Proj 01 added

---
+++ PROJ_01/src/KeyConstructors.java
@@ -0,0 +1,37 @@
+package ist.meic.pa;
+
+import javaassist.*;
+import java.lang.reflect.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class KeyConstructors {
+	private Map<String, Method> setups = new HashMap<String, Method>();
+
+	public KeyConstructors() {
+
+	}
+
+	public void buildConstructors() {
+		ClassPool pool = ClassPool.getDefault();
+		for(CtClass ctClass : pool.get("Widget");
+	}
+
+
+
+
+	public static void main(String[] args) throws Exception {
+		// TO DO: read test from args
+		
+		Scanner console = new Scanner(System.in);
+		String[] commands = console.nextLine().split(" ");
+		
+		KeyConstructors keyC = new KeyConstructors();
+		keyC.buildConstructors();
+		
+		Class testClass = Class.forName(commands[0]);		Method main = testClass.getMethod("main", String[].class);		Constructor[] constructors = testClass.getDeclaredConstructors();		System.out		main.invoke(null, new String[1]);
+		
+		console.close();
+	}
+}
