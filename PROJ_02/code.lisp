88c7b73a8142f17a5a180504700f338ba30c5b33
DanielFigueira <daniel.figueira@ist.utl.pt>
2017-05-04 12:33:00 +0100

    Small changes. List of problems added.

--- PROJ_02/code.lisp
+++ PROJ_02/code.lisp
@@ -1,13 +1,35 @@
-;exemplo de uma macro que recebe um symbolo como argumento
-(defmacro defvar* (vars initial-value)
-  `(progn
-     ,@(loop for var in vars
-             do (check-type var symbol)
-             collect `(defvar ,var ,initial-value))))
-             
-;use (macroexpand 'macro) to check macro
+;;;;;Problems;;;;;
+;make-object: list of field values may not be in order; 
+;			  what about fields that were not present on the list?
+;get-class: crashes (solution: remove 'eval' or use strings for class-name)
+;
 
-;;;;;;;;;;;;;;;;;;;;;;; Meta-class definition ;;;;;;;;;;;;;;;;;;;;;;;,
+;;;;;Considerations;;;;;
+;Strings vs. Symbols for field names
+;get-class-name -> class-name (just like class-fields and class-superclasses)
+;values-in-class -> valid-values
+;replace dolist's for mapcar's (teacher's suggestion)
+;Hashtables
+;Why meta-instance & meta-class? (class Class would be a metaclass, class Person is just a class)
+;
+
+;;;;;Missing;;;;;
+;Getters
+;Recognizer
+;Inheritance ??
+;Save classes somewhere in the environment
+;...
+;
+
+;;;;;;;;;;;;;;;;;;;;;;; Macro definition ;;;;;;;;;;;;;;;;;;;;;;;
+(defmacro def-class (class-name fields superclasses)
+  `(progn
+		;generate class
+		;generate constructor
+		;generate getters
+	))
+
+;;;;;;;;;;;;;;;;;;;;;;; Meta-class definition ;;;;;;;;;;;;;;;;;;;;;;;
 (defun make-class (class-name fields superclasses)
 	(let ((metaclass 
 		(list 
@@ -35,7 +57,7 @@
 		(list (def-symbol (get-class-name class))  ; The given class  !!This works but it's wrong. The hash table would be better
 			field-values    					; The fields values
 			(make-super-objects class fields) ; A list with the superclasses instances
-			))) 
+			)))
 
 
 (defun make-super-objects (class fields)
@@ -49,25 +71,25 @@
 (defun values-in-class (class fields)
 	(mapcan
 		(lambda (field)
-			(if (field-in-class? (car field) class)
+			(if (field-in-class? class (car field))
 				(cdr field)
 				NIL))
 		fields))
 
-(defun field-in-class? (field-name class)
-	(contains? field-name (class-fields class)))
+(defun field-in-class? (class field-name)
+	(contains? (class-fields class) field-name))
 
-;;;;;;;;;;;;;;;;;;;;; Reflective methods
+;;;;;;;;;;;;;;;;;;;;; Reflective methods ;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun get-class (object)
 	(eval (car object)))
 
-;;;;;;;;;;;;;;;;;;;;;; Constructor generation ;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;; Method Generation Functions ;;;;;;;;;;;;;;
 
 (defmacro generate-constructor (class)
 	`(defun ,(intern (string-upcase (concatenate 'string' "make-" (get-class-name (eval class))))) ;function name
 		(&key ,@(mapcar #'def-symbol (class-fields (eval class)))) ;; key arguments
-		(make-object ,class 
+		(make-object ,class
 			(list ,@(mapcar 
 				(lambda(field) `(list ,field (eval,(get-symbol field)))) 
 				(class-fields (eval class))))))) ;;TODO use function get-all-fields
@@ -76,8 +98,8 @@
 
 
 ;;;;;;;;;;;;  Auxiliary methods 
-(defun contains? (el sequence)
-	(find el sequence :test 'equal))
+(defun contains? (sequence elem)
+	(find elem sequence :test 'equal))
 
 (defun def-symbol (symbol)
 	(intern (string-upcase symbol)))
