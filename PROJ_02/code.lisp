39cda0b0e44dfdfe63d2866fef83c837d4174d8e
DanielFigueira <daniel.figueira@ist.utl.pt>
2017-05-12 16:02:17 +0100

    Macro done, all problems fixed

--- PROJ_02/code.lisp
+++ PROJ_02/code.lisp
@@ -1,109 +1,116 @@
-;;;;;Problems;;;;;
-;make-object: list of field values may not be in order; 
-;			  what about fields that were not present on the list?
-;get-class: crashes (solution: remove 'eval' or use strings for class-name)
-;
-
-;;;;;Considerations;;;;;
-;Strings vs. Symbols for field names
-;get-class-name -> class-name (just like class-fields and class-superclasses)
-;values-in-class -> valid-values
-;replace dolist's for mapcar's (teacher's suggestion)
-;Hashtables
-;Why meta-instance & meta-class? (class Class would be a metaclass, class Person is just a class)
-;
+;;;;;Decision Made;;;;;
+; - Global Hashtable with classes (ClassPool)
+; - Keep class & field names as strings instead of symbols <- symbols might be better
+; - Classes and Instances are lists (vs Hashtables)
+; - Fields are kept in order (each field has a slot)
+; - Value assigment order
 
 ;;;;;Missing;;;;;
 ;Getters
 ;Recognizer
-;Inheritance ??
-;Save classes somewhere in the environment
-;...
+;Inheritance:
+;	- Check if superclass name is valid in make-class
+;   - Get parent class fields when calling make-object (generate-object)
+;Multiple Inheritance:
+;	-Test
+;	-...
 ;
 
 ;;;;;;;;;;;;;;;;;;;;;;; Macro definition ;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro def-class (class-name fields superclasses)
-  `(progn
-		;generate class
-		;generate constructor
-		;generate getters
-	))
+(defmacro def-class (classes &rest fields)
+	(let* (
+		(classlist (to-list classes))
+		(class (make-class (car classlist) fields (cdr classlist))))
+		`(progn
+			(generate-constructor ',class)
+			;generate getters
+			',class)))
+		
+(defmacro generate-constructor (class)
+	`(defun ,(new-symbol "make-" (get-class-name (eval class))) ;function name
+		(&key ,@(mapcar #'new-symbol (get-class-fields (eval class)))) ;; key arguments
+		(make-object ,class
+			(list ,@(mapcar
+				(lambda(field) `(list ,field ,(new-symbol field)))
+				(get-class-fields (eval class))))))) ;;TODO: get fields from parent class
 
-;;;;;;;;;;;;;;;;;;;;;;; Meta-class definition ;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;; Class-Related Functions ;;;;;;;;;;;;;;;;;;;;;;;
+(setq classpool (make-hash-table :test 'equal))
+
 (defun make-class (class-name fields superclasses)
-	(let ((metaclass 
-		(list 
-			class-name
+	(let ((class (list
+			(string-upcase class-name)
 			(mapcar #'string-upcase fields)
-			superclasses)))
-		; TODO: create hashtable if needed
-		metaclass))
+			(mapcar #'string-upcase superclasses))))
+		(setf (gethash (get-class-name class) classpool) class)))
 
 (defun get-class-name (class)
 	(car class))
 
-;;; Meta-class accessors
-(defun class-fields (class)
+(defun get-class-fields (class)
 	(cadr class))
 
-(defun class-superclasses (class)
+(defun get-class-superclasses (class)
 	(caddr class))
 
-;;;;;;;;;;;;;;;;;;;;;;; Meta-instance definition ;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;; Instance-Related Functions ;;;;;;;;;;;;;;;;;;;;;;
 
-;;Creates an object instance from a given class and a list of (field-name field-value) pairs
-(defun make-object (class fields) 
-	(let ((field-values (values-in-class class fields)))
-		(list (def-symbol (get-class-name class))  ; The given class  !!This works but it's wrong. The hash table would be better
-			field-values    					; The fields values
-			(make-super-objects class fields) ; A list with the superclasses instances
-			)))
+; Creates an instance from a given class and a list of (field-name field-value) pairs
+(defun make-object (class valuelist)
+	(list (get-class-name class)
+		(multiple-value-bind (values rest) (assign-values class valuelist)
+			(setf valuelist rest)
+			values)
+		(make-super-objects class valuelist)
+		))
 
+; Creates an instance for each superclass of the given class
+(defun make-super-objects (class valuelist)
+	(mapcar
+		(lambda (superclass)
+			(make-object (gethash superclass classpool) valuelist))
+		(get-class-superclasses class)))
 
-(defun make-super-objects (class fields)
-	(let ((instances '()))
-		(dolist (superclass (class-superclasses class))
-			(setf instances (cons
-				(make-object superclass fields)
-				instances)))
-		instances))
+; *** multiple return values ***
+; Returns a list with the values for each field of the class (or NIL; in the field-order)
+; and a list of the values that couldn't be assigned
+(defun assign-values (class valuelist)
+	(values
+		(mapcar
+			(lambda (field)
+				(multiple-value-bind (value rest) (assign-value field valuelist)
+					(setf valuelist rest)
+					value))
+			(get-class-fields class))
+		valuelist))
 
-(defun values-in-class (class fields)
-	(mapcan
-		(lambda (field)
-			(if (field-in-class? class (car field))
-				(cdr field)
+; *** multiple return values ***
+; Returns the correct value for the given field (or NIL) and the list of values without the value used
+(defun assign-value (field valuelist)
+	(values
+		(dolist (value valuelist)
+			(if (equal field (car value))
+				(progn
+					(setf valuelist (remove value valuelist))
+					(return (cadr value)))
 				NIL))
-		fields))
-
-(defun field-in-class? (class field-name)
-	(contains? (class-fields class) field-name))
-
-;;;;;;;;;;;;;;;;;;;;; Reflective methods ;;;;;;;;;;;;;;;;;;;;;;;;
+		valuelist))
 
 (defun get-class (object)
-	(eval (car object)))
+	(values (gethash (car object) classpool))) ;keep only first value
 
-;;;;;;;;;;;;;;;;;;;;;; Method Generation Functions ;;;;;;;;;;;;;;
+;;;;;;;;;;;;  Auxiliary methods
+(defun to-list (elem)
+	(if (listp elem) elem (list elem)))
 
-(defmacro generate-constructor (class)
-	`(defun ,(intern (string-upcase (concatenate 'string' "make-" (get-class-name (eval class))))) ;function name
-		(&key ,@(mapcar #'def-symbol (class-fields (eval class)))) ;; key arguments
-		(make-object ,class
-			(list ,@(mapcar 
-				(lambda(field) `(list ,field (eval,(get-symbol field)))) 
-				(class-fields (eval class))))))) ;;TODO use function get-all-fields
-
-
-
-
-;;;;;;;;;;;;  Auxiliary methods 
-(defun contains? (sequence elem)
-	(find elem sequence :test 'equal))
-
-(defun def-symbol (symbol)
-	(intern (string-upcase symbol)))
-
-(defun get-symbol (symbol)
-	(intern (string-upcase symbol)))
-
+(defun new-symbol (str1 &optional str2)
+	(intern (string-upcase (concatenate 'string str1 str2))))
+	
+;;;;;;;;;;;;;;;;;;;;; Test Objects ;;;;;;;;;;;;;;;;;;;;;;;;
+(setf human (def-class human bloodtype))
+(setf person (def-class (person human) name age))
+(setf student (def-class (student person) course school))
+(setf carlos (make-human :bloodtype "AB"))
+(setf joao (make-person :age 20 :name "Joao")) ; field order is irrelevant
+(setf maria (make-student :course "Economy")) ; missing fields are set to NIL
