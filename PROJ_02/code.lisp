85993889fd56c6d3495e9fe77ec7745ccebf02fe
DanielFigueira <daniel.figueira@ist.utl.pt>
2017-05-19 07:06:49 +0100

    Getter working fine. Tested, cleaned and commented.

--- PROJ_02/code.lisp
+++ PROJ_02/code.lisp
@@ -1,17 +1,6 @@
-;;;;;Decisions Made;;;;;
-; - Global Hashtable with classes (ClassPool)
-; - Keep class & field names as strings instead of symbols <- symbols might be better
-; - Classes and Instances are structured as lists
-; - Fields are kept in order (each field has a slot)
-; - Value assignment by class levels
-
-;;;;;Missing;;;;;
-; Getters
-; Recognizer
-; Run Tests
-; Extension
 
 ;;;;;;;;;;;;;;;;;;;;;;; Macro definition ;;;;;;;;;;;;;;;;;;;;;;;
+
 (defmacro def-class (classes &rest fields)
 	(let* (
 		(classlist (to-list classes))
@@ -20,156 +9,132 @@
 			(generate-constructor ',class)
 			(generate-recognizer ',class)
 			(generate-getters ',class)
-			(generate-setters ',class)
 			',class)))
 		
 (defmacro generate-constructor (class)
 	`(defun ,(new-symbol "make-" (get-name (eval class)))
-		(&key ,@(mapcar #'new-symbol (get-all-fields (eval class))))
+		(&key ,@(mapcar #'new-symbol (unique (get-all-fields (eval class)))))
 		(make-object ,class
 			(list ,@(mapcar
 				#'(lambda (field) `(list ,field ,(new-symbol field)))
-				(get-all-fields (eval class)))))))
+				(unique (get-all-fields (eval class))))))))
 
 (defmacro generate-recognizer (class)
 	`(defun ,(new-symbol (get-name (eval class)) "?")
 		(object)
-		(if (listp object) ; check if is a list
-			(let ((class (get-class object)))
-				(if class ; check if first element is the name of a valid class
-					(contains (get-all-classes class) ,(get-name (eval class))))))))
+		(if (listp object) ; check if object is a list
+			(let ((objClass (get-class object)))
+				(if objClass ; check if its first element is the name of a valid class
+					(contains (get-all-classnames objClass) ,(get-name (eval class))))))))
 				
 (defmacro generate-getters (class)
-	(let ((counter -1))
-		`(progn
-			,@(mapcar
-				#'(lambda (field)
-					(progn
-						(incf counter)
-						`(defun ,(new-symbol (get-name (eval class)) "-" field)
-							(object)
-							(if (,(new-symbol (get-name (eval class)) "?") object)
-								(nth ,counter (get-fields object))))))
-			(get-fields (eval class))))))
-			
-(defmacro generate-setters (class)
-	(let ((counter -1))
-		`(progn
-			,@(mapcar
-				#'(lambda (field)
-					(progn
-						(incf counter)
-						`(defun ,(new-symbol (get-name (eval class)) "-" field "!")
-							(object value)
-							(if (,(new-symbol (get-name (eval class)) "?") object)
-								(set-field object ,counter value))
-							object)))
-			(get-fields (eval class))))))
-			
-	  
+	`(progn
+		,@(mapcar
+			#'(lambda (field)
+				`(defun ,(new-symbol (get-name (eval class)) "-" field)
+					(object)
+					(if (,(new-symbol (get-name (eval class)) "?") object)
+						(nth
+							,(find-index (get-all-fields (eval class)) field)
+							(get-all-values (get-object-by-name object ,(get-name (eval class))))))))
+			(get-all-fields (eval class)))))
+		
 
 ;;;;;;;;;;;;;;;;;;;;;;; Class-Related Functions ;;;;;;;;;;;;;;;;;;;;;;;
-(setq classpool (make-hash-table :test 'equal))
 
+(setf classpool (make-hash-table :test 'equal))
+
+; Returns a new class and adds it to the ClassPool
 (defun make-class (class-name fields superclasses)
 	(let ((class (list
 			(string-upcase class-name)
-			(mapcar #'string-upcase fields)	; validate fields?
-			(mapcar #'string-upcase superclasses)))) ; validate superclasses?
+			(mapcar #'string-upcase fields)
+			(mapcar #'string-upcase superclasses))))
 		(setf (gethash (get-name class) classpool) class)))
-			
 
-(defun get-name (class) ;works for classes and instances
-	(copy-seq (car class))) ;protected
+; Return a *copy* of the name of the given class
+(defun get-name (class)
+	(copy-seq (car class)))
 
-(defun get-fields (class) ;works for classes and instances
-	(copy-list (cadr class))) ;protected
+; Return a *copy* of the fields of the given class  (list)
+(defun get-fields (class)
+	(copy-list (cadr class)))
 
-(defun set-field (class field value) ;works for classes and instances
-	(set-nth (cadr class) field value))	
-	
-(defun get-superclasses (class) ;works for classes and instancess
-	(copy-list (caddr class))) ;protected
+; Return a *copy* of the names of the superclasses of the given class (list)
+(defun get-superclasses-name (class)
+	(copy-list (caddr class)))
 
-; Returns a unique list of the given class and its superclasses fields (DFS)
+; Returns a list of the superclasses of the given class
+(defun get-superclasses (class)
+	(mapcar
+		#'(lambda (classname)
+			(gethash classname classpool))
+		(get-superclasses-name class)))
+
+; Returns a list of the given class and its superclasses fields (BFS)
 (defun get-all-fields (class)
-	(remove-duplicates
-		(mapcan
-			#'(lambda (class-name)
-				(get-fields (gethash class-name classpool)))
-			(get-all-classes class))
-		:test #'equal :from-end t))
+	(mapcan
+		#'(lambda (classname)
+			(get-fields (gethash classname classpool)))
+		(get-all-classnames class)))
 
-; Returns a list of the given class and its superclasses names (DFS)
-(defun get-all-classes (class)
-	(let ((superclasses (get-superclasses class)))
-		(if (null superclasses)
-			(to-list (get-name class))
-			(cons (get-name class)
-				(mapcan
-				#'(lambda (superclass-name)
-					(get-all-classes (gethash superclass-name classpool)))
-				superclasses)))))
+; Returns a list of the given class and its superclasses names (BFS)
+(defun get-all-classnames (class)
+	(BFS class #'get-superclasses #'get-name))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;; Instance-Related Functions ;;;;;;;;;;;;;;;;;;;;;;
-
-; Creates an instance from a given class and a list of (field-name field-value) pairs
+		
+; Creates an instance of a given class and assigns its initial values
+; based on a list of <field-name, field-value> pairs
 (defun make-object (class valuelist)
+	(let ((newObject (make-new-object class))) ; start with an empty class
+		(mapcar ; for each object hierarchy (BFS)
+			#'(lambda (object)
+				(setf (cadr object)
+					(mapcar ; for each field in its class
+						#'(lambda (field)
+							(dolist (pair valuelist) ; search valuelist and assign value
+								(if (equal field (car pair))
+									(progn
+										(setf valuelist (remove pair valuelist))
+										(return (cadr pair))))))
+						(get-fields (get-class object)))))
+			(get-all-objects newObject))
+		newObject))
+
+; Returns an instance of a class without any fields
+(defun make-new-object (class)
 	(list (get-name class)
-		(multiple-value-bind (values rest) (assign-values class valuelist)
-			(setf valuelist rest)
-			values)
-		(make-super-objects class valuelist)
-		))
-
-; Creates an instance for each superclass of the given class
-(defun make-super-objects (class valuelist)
-	(mapcar
-		#'(lambda (superclass)
-			(make-object (gethash superclass classpool) valuelist))
-		(get-superclasses class)))
-
-; *** multiple return values ***
-; Returns a list with the values for each field of the class (or NIL; in the field-order)
-; and a list of the values that couldn't be assigned
-(defun assign-values (class valuelist)
-	(values
+		NIL ; Leave empty
 		(mapcar
-			#'(lambda (field)
-				(multiple-value-bind (value rest) (assign-value field valuelist)
-					(setf valuelist rest)
-					value))
-			(get-fields class))
-		valuelist))
+			#'(lambda (superclass)
+				(make-new-object (gethash superclass classpool)))
+			(get-superclasses-name class))))
 
-; *** multiple return values ***
-; Returns the correct value for the given field (or NIL) and the list of values without the value used
-(defun assign-value (field valuelist)
-	(values
-		(dolist (value valuelist)
-			(if (equal field (car value))
-				(progn
-					(setf valuelist (remove value valuelist))
-					(return (cadr value)))
-				NIL))
-		valuelist))
-
+; Returns the class of the given instance
 (defun get-class (object)
-	(values (gethash (car object) classpool))) ;keep only first value
+	(values (gethash (car object) classpool)))
 
-; ; Returns a list of the given class and its superclasses names (DFS)
-; (defun get-all-values (object)
-; 	(let ((superobjects (get-superclasses object))
-; 		(values (get-fields object))) ; leave superobjects out
-; 		(if (null superobjects)
-; 			(to-list values)
-; 			(cons values
-; 				(mapcar
-; 				#'(lambda (superobject)
-; 					(get-all-values superobject))
-; 				superobjects)))))
+; Returns a list of the given instance and its superinstances values (BFS)
+(defun get-all-values (object)
+	(mapcan #'get-fields (get-all-objects object)))
+
+; Returns a list of the given instance and its superinstances (BFS)
+(defun get-all-objects (object)
+	(BFS object #'get-superclasses-name #'to-list))
+
+; Returns the first instance of the given classname found on the hierarchy of the given object
+(defun get-object-by-name (object classname)
+	(dolist (obj (get-all-objects object))
+		(if (equal (get-name obj) classname)
+			(return obj))))
 	
-;;;;;;;;;;;;  Auxiliary methods
+	
+;;;;;;;;;;;;;;;;;;;;;;;;  Auxiliary methods  ;;;;;;;;;;;;;;;;;;;
+(defun unique (lst)
+	(remove-duplicates lst :test #'equal :from-end t))
+
 (defun to-list (elem)
 	(if (listp elem) elem (list elem)))
 
@@ -179,45 +144,44 @@
 (defun contains (lst elem)
 	(not (not (member elem lst :test #'equal)))) ; convert to boolean
 
-(defun set-nth (list n val)
-  (if (> n 0)
-      (cons (car list)
-            (set-nth (cdr list) (1- n) val))
-      (rplaca list val)))
+(defun find-index (lst elem)
+	(position elem lst :test #'equal))
 
-; Returns the result of concatenating all strings in a list
 (defun string-conc (strings)
 	(let ((result ""))
 		(dolist (s strings)
 			(setq result (concatenate 'string result s)))
 		result))
 
+
 ;;;;;;;;;;;;;;;;;;;;;;; Search Algorithms ;;;;;;;;;;;;;;;;;;;;;;;
 
-;childNodeFunc - must return a list of child nodes
+; Best First Search Algorithm Implementation
+;startNode - starting
+;childNodeFunc - function used to get a list of child nodes (takes the node as arg)
+;execFunc - function to be applied to each node (takes node + given args as args)
+;args - additional arguments to be passed to execFunc
+(defun BFS (startNode childNodeFunc execFunc &rest args)
+	(let
+		((stack (list startNode))
+		(node startNode)
+		(childNodes NIL))
+		(loop while (not (null stack)) do
+			(setf node (car stack))
+			(setf stack (cdr stack))
+			(setf childNodes (to-list (apply childNodeFunc (list node))))
+			(if childNodes
+				(setf stack (append stack childNodes)))
+		collect (apply execFunc (append (list node) args)))))
 
-; (defun DFS (startNode childNodeFunc execFunc)
-; 	(let
-; 		((stack '(startNode))
-; 		(node startNode))
-; 		(loop while (not (null stack)) do
-; 			(progn
-; 				(setf node (car stack))
-; 				(setf stack (cdr stack))
-; 				(setf stack (append stack (apply childNodeFunc (list node)))
-; 				(setf stack (append stack (apply childNodeFunc (list node)))
-; 				(write stack)
-; 				(write-line "")
-; 			))
-; ))))
 
 ;;;;;;;;;;;;;;;;;;;;; Test Objects ;;;;;;;;;;;;;;;;;;;;;;;;
-(setf entity (def-class entity)) ; circular inheritance
-(setf human (def-class (human entity) bloodtype))
-(setf hero (def-class (hero entity) power))
-(setf person (def-class (person human hero) name age)) ; multiple inheritance
-(setf student (def-class (student person) course))
-(setf superman (make-hero :power "strength"))
-(setf carlos (make-human :bloodtype "AB"))
-(setf joao (make-person :bloodtype "AB" :age 20 :name "Joao" :power "Flying")) ; field order is irrelevant
-(setf maria (make-student :course "Economy" :age 13 :name "Maria" :power "Invisibility")) ; missing fields are set to NIL
+; (setf entity (def-class entity foo)) ; circular inheritance
+; (setf human (def-class (human entity) bloodtype))
+; (setf hero (def-class (hero entity) power))
+; (setf person (def-class (person human hero) name age)) ; multiple inheritance
+; (setf student (def-class (student person) course))
+; (setf superman (make-hero :power "strength"))
+; (setf carlos (make-human :bloodtype "AB"))
+; (setf joao (make-person :bloodtype "AB" :age 20 :name "Joao" :power "Flying")) ; field order is irrelevant
+; (setf maria (make-student :course "Economy" :age 13 :name "Maria" :power "Invisibility")) ; missing fields are set to NIL
